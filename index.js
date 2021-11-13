e403056329cdce9237315b704cc28d19663cbfb0
tajbiul-hossain <tbh.nishat@gmail.com>
2021-11-13 19:14:44 +0600

    upsert user added

--- index.js
+++ index.js
@@ -177,6 +177,7 @@ async function run() {
 
     app.put("/users", async (req, res) => {
       const user = req.body;
+      user.role = "user";
       const filter = { email: user.email };
       const options = { upsert: true };
       const updateDoc = { $set: user };
